b1f50355dd69d13b29a45244d94be9811ac2a3ef
cuiqilin <1732249216@qq.com>
2024-07-03 23:02:56 +0800

    mine-layout

---
+++ components/mine-tool/mine-tool.js
@@ -0,0 +1,24 @@
+// components/mine-tool/mine-tool.js
+Component({
+
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        toolItem: Object
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
